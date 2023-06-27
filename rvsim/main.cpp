4a0e25f1dd78f9f6323e7f178249420238ba0924
Ahmed-A <ahmed-a@aucegypt.edu>
2023-06-27 21:13:07 +0300

    Add project files.

---
+++ rvsim/main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+	This is just a skeleton. It DOES NOT implement all the requirements.
+	It only recognizes the RV32I "ADD", "SUB" and "ADDI" instructions only.
+	It prints "Unkown Instruction" for all other instructions!
+
+	References:
+	(1) The risc-v ISA Manual ver. 2.1 @ https://riscv.org/specifications/
+	(2) https://github.com/michaeljclark/riscv-meta/blob/master/meta/opcodes
+*/
+
+#include <iostream>
+#include <fstream>
+#include "stdlib.h"
+#include <iomanip>
+
+using namespace std;
+
+unsigned int pc;
+unsigned char memory[(16 + 64) * 1024];
+
+void emitError(char* s)
+{
+	cout << s;
+	exit(0);
+}
+
+void printPrefix(unsigned int instA, unsigned int instW) {
+	cout << "0x" << hex << std::setfill('0') << std::setw(8) << instA << "\t0x" << std::setw(8) << instW;
+}
+
+void instDecExec(unsigned int instWord)
+{
+	unsigned int rd, rs1, rs2, funct3, funct7, opcode;
+	unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
+	unsigned int address;
+
+	unsigned int instPC = pc - 4;
+
+	opcode = instWord & 0x0000007F;
+	rd = (instWord >> 7) & 0x0000001F;
+	funct3 = (instWord >> 12) & 0x00000007;
+	rs1 = (instWord >> 15) & 0x0000001F;
+	rs2 = (instWord >> 20) & 0x0000001F;
+
+	// — inst[31] — inst[30:25] inst[24:21] inst[20]
+	I_imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
+
+	printPrefix(instPC, instWord);
+
+	if (opcode == 0x33) {		// R Instructions
+		switch (funct3) {
+		case 0: if (funct7 == 32) {
+			cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		}
+			  else {
+			cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		}
+			  break;
+		default:
+			cout << "\tUnkown R Instruction \n";
+		}
+	}
+	else if (opcode == 0x13) {	// I instructions
+		switch (funct3) {
+		case 0:	cout << "\tADDI\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n";
+			break;
+		default:
+			cout << "\tUnkown I Instruction \n";
+		}
+	}
+	else {
+		cout << "\tUnkown Instruction \n";
+	}
+
+}
+
+int main(int argc, char* argv[]) {
+
+	unsigned int instWord = 0;
+	ifstream inFile;
+	ofstream outFile;
+
+	if (argc < 1) emitError("use: rvcdiss <machine_code_file_name>\n");
+
+	inFile.open(argv[1], ios::in | ios::binary | ios::ate);
+
+	if (inFile.is_open())
+	{
+		int fsize = inFile.tellg();
+
+		inFile.seekg(0, inFile.beg);
+		if (!inFile.read((char*)memory, fsize)) emitError("Cannot read from input file\n");
+
+		while (true) {
+			instWord = (unsigned char)memory[pc] |
+				(((unsigned char)memory[pc + 1]) << 8) |
+				(((unsigned char)memory[pc + 2]) << 16) |
+				(((unsigned char)memory[pc + 3]) << 24);
+			pc += 4;
+			// remove the following line once you have a complete simulator
+			if (pc == 32) break;			// stop when PC reached address 32
+			instDecExec(instWord);
+		}
+	}
+	else emitError("Cannot access input file\n");
+}
