f724254e42fe761cc3fbb5056fd84e0c622820f0
OmarEflouly <omarelfouly@aucegypt.edu>
2023-06-30 22:39:31 +0300

    Fixed J_imm

--- rvsim/main.cpp
+++ rvsim/main.cpp
@@ -46,7 +46,7 @@ void instDecExec(unsigned int instWord)
 	// â€” inst[31] â€” inst[30:25] inst[24:21] inst[20]
 	I_imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
 	S_imm = ((funct7 <<5) | rd) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
-	J_imm = ((instWord>>21)&0x3FF) | ((instWord>>20)&0x1) | ((instWord>>12)&0xFF) | ((instWord>>31)?0xFFF80000:0x0);
+	J_imm = (((instWord>>21)&0x3FF)<<1) | (((instWord>>20)&0x1)<<11) | (((instWord>>12)&0xFF)<<12) | ((instWord>>31)?0xFFF80000:0x0);
 
 	printPrefix(instPC, instWord);
 
